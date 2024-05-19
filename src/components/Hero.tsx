4d939619f05e5c33c7fc61fb034450209ff96ffc
Piyush Paul <piyushpaul605@gmail.com>
2024-05-19 15:07:50 +0530

    created Chat

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,16 @@
+import { Inter } from "next/font/google";
+import Chat from "./Chat";
+
+export const inter = Inter({
+    subsets: ['latin'], 
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
+
 export default function Hero(){
     return (
-        <h1>Hello World</h1>
+        <div className={`${inter.className} flex`}>
+            <h1>Hello World</h1>
+            <Chat/>
+        </div>
     )
 } 
